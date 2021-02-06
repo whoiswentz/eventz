70619bef232c3e585321941f2c75ce574e1d0562
Vinicios Wentz <vinicios@wentz.io>
2021-02-06 11:27:33 -0300

    feature: showing only the upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ApplicationRecord
-    def free?
-        price.blank? || price.zero?
-    end
+  def self.upcoming
+    where('starts_at > ?', Time.now).order('starts_at desc')
+  end
+
+  def free?
+    price.blank? || price.zero?
+  end
 end
