b53fc63535eed584015309c6d4aae14e377ef0b8
Vinicios Wentz <vinicios@wentz.io>
2021-02-08 21:52:38 -0300

    feature: user signup form and logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   root 'events#index'
 
   resources :events do
     resources :registrations
   end
+
+  resources :users
+  get 'signup', to: 'users#new'
+
 end
