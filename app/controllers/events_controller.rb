31b3cc9dafbdba628dbf681bad680f3ed666ab93
Vinicios Wentz <vinicios@wentz.io>
2023-12-10 23:10:56 -0300

    Forms: Creating Records

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,16 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update]
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
   def index
     @events = Event.all
   end
@@ -12,13 +22,13 @@ class EventsController < ApplicationController
   end
 
   def update
-    @event.update(update_params)
+    @event.update(event_params)
     redirect_to @event
   end
 
   private
 
-  def update_params
+  def event_params
     params.require(:event)
       .permit(:name, :description, :location, :price, :starts_at)
   end
