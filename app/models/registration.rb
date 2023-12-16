bbc57a2bf952eb619455e4e6588df38e7e0aa54b
Vinicios Wentz <vinicios@wentz.io>
2023-12-15 23:09:01 -0300

    One-to-Many: has_many

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,16 @@
 class Registration < ApplicationRecord
+  HOW_HEARD_OPTIONS = [
+    'Newsletter',
+    'Blog Post',
+    'Twitter',
+    'Web Search'
+    'Friend/Coworker',
+    'Other'
+  ]
+
   belongs_to :event
+
+  validates :name, presence: true
+  validates :email, format: { with: /\S+@\S+/ }
+  validates :how_heard, inclusion: { in: HOW_HEARD_OPTIONS }
 end
