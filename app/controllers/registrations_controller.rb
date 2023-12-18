2bd8d859517cc16ac41404f51d653c9bd6924265
Vinicios Wentz <vinicios@wentz.io>
2023-12-18 17:34:14 -0300

    One-to-Many: Forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,30 @@
 class RegistrationsController < ApplicationController
+  before_action :find_event, only: [:index, :new, :create]
+
   def index
-    @event = Event.find(params[:event_id])
     @registrations = @event.registrations
+  end
+
+  def new
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @registration = @event.registrations.new(registration_params)
+    if @registration.save
+      redirect_to event_registrations_url(@event), notice: "Thanks for register!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def registration_params
+    params.require(:registration).permit(:name, :email, :how_heard)
   end
 end
