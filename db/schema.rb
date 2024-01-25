626183f0f5971ad42f8512dd3d676577da333169
Vinicios Wentz <vinicios@wentz.io>
2024-01-24 22:22:36 -0300

    User Account Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_16_010725) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_22_135607) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -31,6 +31,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_16_010725) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_registrations_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "registrations", "events"
